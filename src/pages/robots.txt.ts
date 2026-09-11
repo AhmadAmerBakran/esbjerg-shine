21c0e6756be99ff8be41eb2e3612d95df17c3722
AhmadAmerBakran <114908029+AhmadAmerBakran@users.noreply.github.com>
2026-09-11 20:14:23 +0200

    feat: add robots endpoint

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,8 @@
+export const prerender = true;
+
+export function GET({ site }: { site?: URL }) {
+  const origin = (site ?? new URL('https://esbjergshine.dk')).toString().replace(/\/$/, '');
+  return new Response(`User-agent: *\nAllow: /\n\nSitemap: ${origin}/sitemap.xml\n`, {
+    headers: { 'Content-Type': 'text/plain; charset=utf-8' }
+  });
+}
